82a3ab1167639df0e17dbb2bd781dc9c9c9727c9
Edward Hambleton <ehambleton@me.com>
2017-12-14 01:47:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,18 @@ function addElementToBeginningOfArray(array,element) {
 var newArray = [element, ...array]
 return newArray
 }
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+array.unshift(element)
+return array
+}
+
+function addElementToEndOfArray(array,element) {
+var newArray = [...array, element]
+return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+array.shift(element)
+return array
+}
