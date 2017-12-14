03a2fc3af2dbc227614e57adaf9cb3ab30ab3588
Edward Hambleton <ehambleton@me.com>
2017-12-14 01:24:27 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var thisArray
+
+function addElementToBeginningOfArray(thisArray, Element) {
+  newArray = thisArray.unshift(Element)
+  return newArray
+}
+
+fuction destructivelyAddElementToBeginningOfArray(thisArray, Element)
+thisArray.unshift(Element)
+return thisArray
+}
